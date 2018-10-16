8bc8538bfa56deca5d347c9d64d81d44b096de45
Tamás Németh <tomuwhu@gmail.com>
2018-10-16 20:33:18 +0200

    .

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -2,18 +2,18 @@
   <v-app>
     <v-container class="text-xs-center">
     <div v-if="edit">  
-    <h1 v-if="o._id">Szerkesztés</h1>  
-    <h1 v-else>Új felvitel</h1>  
-    <v-layout class="row wrap">  
-      <v-flex class="td" xs12 offset-sm1 offset-md1 sm5 md5 lg4 offset-lg2>  
-        <v-text-field v-model="o.nev" label="Név"/>
-      </v-flex>
-      <v-flex  class="td" xs12 sm5 md5 lg4>
-        <v-select v-model="o.select" label="Nomi" :items="['Cumó','Cica']" multiple/>
-      </v-flex>
-    </v-layout>
-    <v-btn color="warning" @click="edit=false">Mégse</v-btn>
-    <v-btn color="success" @click="f">Mentés</v-btn>
+      <h1 v-if="o._id">Szerkesztés</h1>  
+      <h1 v-else>Új felvitel</h1>  
+      <v-layout class="row wrap">  
+        <v-flex class="td" xs12 offset-sm1 offset-md1 sm5 md5 lg4 offset-lg2>  
+          <v-text-field v-model="o.nev" label="Név"/>
+        </v-flex>
+        <v-flex  class="td" xs12 sm5 md5 lg4>
+          <v-select v-model="o.select" label="Nomi" :items="['Cumó','Cica']" multiple/>
+        </v-flex>
+      </v-layout>
+      <v-btn color="warning" @click="edit=false">Mégse</v-btn>
+      <v-btn color="success" @click="f">Mentés</v-btn>
     </div>
     <table class="table" v-else>
       <tr><td colspan=4>Cuccok listája</td></tr>
